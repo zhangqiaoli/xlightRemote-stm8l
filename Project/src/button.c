53539c3e44a3d75ceba039c6bdc345a4dd5f06a4
sunbaoshi <sunbaoshi1975@gmail.com>
2017-04-21 13:24:58 -0400

    comment on button layout regarding PCB version

--- Project/src/button.c
+++ Project/src/button.c
@@ -41,12 +41,12 @@ LEDs
 
 // Button pin map
 #define BUTTONS_PORT1           (GPIOD)
-
+// Old PCB before 2017-03-20
 #define BUTTON_PIN_LEFT         (GPIO_Pin_0)
 #define BUTTON_PIN_RIGHT        (GPIO_Pin_1)
 #define BUTTON_PIN_UP           (GPIO_Pin_2)
 #define BUTTON_PIN_DOWN         (GPIO_Pin_3)
-
+// New PCB from 2017-03-20
 /*
 #define BUTTON_PIN_UP           (GPIO_Pin_0)
 #define BUTTON_PIN_DOWN         (GPIO_Pin_1)
